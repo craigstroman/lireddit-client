e6d445dbbe869ede5aa8147ff396c385b7a5dc88
Craig Stroman <cstroman@gmail.com>
2025-07-30 19:48:47 -0400

    Adding login component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Home } from '../Home/Home';
 import { Register } from '../Register/Register';
+import { Login } from '../Login/Login';
 
 export const Main: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
