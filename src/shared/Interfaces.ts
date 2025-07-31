3d139806ecf6e6eb3aa9d0a745fc9457df38253c
Craig Stroman <cstroman@gmail.com>
2025-07-31 15:17:12 -0400

    Adding a new interface

--- src/shared/Interfaces.ts
+++ src/shared/Interfaces.ts
@@ -2,3 +2,12 @@ export interface IFormValues {
   username: string;
   password: string;
 }
+
+export interface InputFieldProps {
+  placeholder: string;
+  name: string;
+  fieldErrors: {
+    [key: string]: string;
+  };
+  touched: any;
+}
