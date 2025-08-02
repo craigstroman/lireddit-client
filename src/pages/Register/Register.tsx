0ffd62d41800805038a66ceefa6efd25452e2f4e
Craig Stroman <cstroman@gmail.com>
2025-08-02 13:32:19 -0400

    Adding new fields to formik

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
+import { gql, useMutation } from 'urql';
 import { InputField } from '../../components/InputField/InputField';
 import { IFormValues } from '../../shared/Interfaces';
 import './Register.scss';
 
 export const Register: React.FC = () => {
+  const REGISTER_MUTATION = useMutation(`
+    mutation Register(
+      $first_name: String!
+      $last_name: String!
+      $email: String!
+      $username: String!
+      $password: String!
+    ) {
+      register(
+        options: {
+          first_name: $first_name
+          last_name: $last_name
+          email: $email
+          username: $username
+          password: $password
+        }
+      ) {
+        user {
+          id
+          createdAt
+          updatedAt
+          username
+        }
+        errors {
+          field
+          message
+        }
+      }
+    }
+  `);
+  const [, register] = useMutation(REGISTER_MUTATION);
   const initialValues: IFormValues = {
+    first_name: '',
+    last_name: '',
+    email: '',
     username: '',
     password: '',
   };
 
   const handleValidation = (values: IFormValues) => {
     const errors: IFormValues = {
+      first_name: '',
+      last_name: '',
+      email: '',
       username: '',
       password: '',
     };
+    if (!values.first_name) {
+      errors.first_name = 'First name is required.';
+    }
+
+    if (!values.last_name) {
+      errors.last_name = 'Last name is required.';
+    }
+
+    if (!values.email) {
+      errors.email = 'Email is required.';
+    }
+
     if (!values.username) {
-      errors.username = 'Username is required';
+      errors.username = 'Username is required.';
     }
 
     if (!values.password) {
-      errors.password = 'Password is Required';
+      errors.password = 'Password is Required.';
     }
     return errors;
   };
@@ -31,6 +81,7 @@ export const Register: React.FC = () => {
         initialValues={initialValues}
         onSubmit={(values, { setSubmitting }) => {
           setSubmitting(false);
+          // register()
         }}
         validate={(values) => handleValidation(values)}
       >
