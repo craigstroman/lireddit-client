944d10abe6f372aa5d895a5322766da3390a01fa
Craig Stroman <cstroman@gmail.com>
2025-08-02 13:00:15 -0400

    Wrapping app in a Graphql provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
 import { Main } from './pages/Main/Main';
 
 const element = document.getElementById('app');
 const root = createRoot(element as HTMLDivElement);
 
+const client = new Client({
+  url: 'http://localhost:9000/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
+
 root.render(
   <React.StrictMode>
-    <Main />
+    <Provider value={client}>
+      <Main />
+    </Provider>
   </React.StrictMode>,
 );
