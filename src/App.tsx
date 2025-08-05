04462ec720750b6ce8a07e6e67d86f3aaecd0f97
Craig Stroman <cstroman@gmail.com>
2025-08-05 13:37:16 -0400

    Updating client apollo code

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ const client = new Client({
   url: 'http://localhost:9000/graphql',
   exchanges: [cacheExchange, fetchExchange],
   fetchOptions: {
-    mode: 'no-cors',
     credentials: 'include',
   },
 });
