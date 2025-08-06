ca6de25027115dd1adfd0cdea7e11ed52f419600
Craig Stroman <cstroman@gmail.com>
2025-08-06 15:47:34 -0400

    Removing a setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
+import { Client, Provider } from 'urql';
 import { Main } from './pages/Main/Main';
 
 const element = document.getElementById('app');
@@ -8,7 +8,6 @@ const root = createRoot(element as HTMLDivElement);
 
 const client = new Client({
   url: 'http://localhost:9000/graphql',
-  exchanges: [cacheExchange, fetchExchange],
   fetchOptions: {
     credentials: 'include',
   },
