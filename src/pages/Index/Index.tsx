a67aa6af19047e64d16cd83152e8e52a53796c7e
Craig Stroman <cstroman@gmail.com>
2025-08-09 14:58:30 -0400

    Creating index page

---
+++ src/pages/Index/Index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavBar } from '../../components/NavBar/NavBar.';
+
+export const Index: React.FC = () => {
+  return (
+    <div>
+      <NavBar />
+      <div>Hello world</div>
+    </div>
+  );
+};
