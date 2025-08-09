6449c26cdcd17baee054c785d207f0a3ebf50f79
Craig Stroman <cstroman@gmail.com>
2025-08-09 13:44:48 -0400

    Adding check if logged in if not redirect to login page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 export const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+  const cookies = new Cookies(null, { path: '/', domain: 'localhost' });
+  const uid = cookies.get('uid');
+
+  useEffect(() => {
+    if (!uid) {
+      navigate('/');
+    }
+  }, [uid]);
+
   return <div>Dashboard</div>;
 };
