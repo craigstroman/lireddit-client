23661fde943f1a1e37466c28120040ef11157379
Craig Stroman <cstroman@gmail.com>
2025-08-10 14:30:05 -0400

    Adding to do comment

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 export const Dashboard: React.FC = () => {
+  // TODO: Come back to this and use me query if I can get it working instead of looking for cookie to confirm person logged in.
   const navigate = useNavigate();
   const cookies = new Cookies(null, { path: '/', domain: 'localhost' });
   const uid = cookies.get('uid');
