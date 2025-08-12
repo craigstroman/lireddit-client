308b127dae1a666dca537a58d5ca8a42547cde7b
Craig Stroman <cstroman@gmail.com>
2025-08-12 13:04:15 -0400

    Creating navbar component

--- src/components/NavBar/NavBar..tsx
+++ src/components/NavBar/NavBar..tsx
@@ -1,18 +1,31 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import Cookies from 'universal-cookie';
+import { useLogoutMutation } from '../../generated/graphql';
 import './NavBar.scss';
 
 export const NavBar: React.FC = () => {
-  const navigate = useNavigate();
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const cookies = new Cookies(null, { path: '/', domain: 'localhost' });
   const uid = cookies.get('uid');
 
-  useEffect(() => {
-    if (!uid) {
-      navigate('/');
-    }
-  }, [uid]);
+  if (uid) {
+    return (
+      <header>
+        <div className="header-content">
+          <button
+            className="logout-button"
+            type="button"
+            onClick={() => {
+              logout();
+              window.location.reload();
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      </header>
+    );
+  }
 
   return (
     <header>
