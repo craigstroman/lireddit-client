34dac8f0ec92bb9bea80afa87c80ac15c221a577
Craig Stroman <cstroman@gmail.com>
2025-08-12 21:18:21 -0400

    Updating dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,23 +1,18 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Cookies from 'universal-cookie';
 import { useMeQuery } from '../../generated/graphql';
 import { NavBar } from '../../components/NavBar/NavBar.';
 import './Dashboard.scss';
 
 export const Dashboard: React.FC = () => {
-  // TODO: Come back to this and use me query if I can get it working instead of looking for cookie to confirm person logged in.
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data }] = useMeQuery();
   const navigate = useNavigate();
-  const cookies = new Cookies(null, { path: '/', domain: 'localhost' });
-  const uid = cookies.get('uid');
 
   useEffect(() => {
-    if (!uid) {
-      console.log('navigate: ');
+    if (!data?.me?.username) {
       navigate('/');
     }
-  }, [uid]);
+  }, [data]);
 
   return (
     <div className="dashboard-container">
