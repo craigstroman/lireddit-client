d4a3edb29e08040db9ea41d7312c978488124d7c
Craig Stroman <cstroman@gmail.com>
2025-08-18 12:14:40 -0400

    Creating toggle password component

---
+++ src/components/TogglePassword/TogglePassword.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { ILoginErrors, IErrors } from '../../shared/Interfaces';
+import './TogglePassword.scss';
+
+export const TogglePassword: React.FC<IErrors> = ({ errors }) => {
+  console.log('errors: ', errors);
+  const [fieldType, setFieldType] = useState<string>('password');
+  const [icon, setIcon] = useState<IconDefinition>(faEyeSlash);
+  const [iconLabel, setIconLabel] = useState<string>('Hide password');
+
+  const togglePasswordView = () => {
+    if (fieldType === 'password') {
+      setIcon(faEye);
+      setFieldType('text');
+      setIconLabel('Show password');
+    } else {
+      setIcon(faEyeSlash);
+      setFieldType('password');
+      setIconLabel('Hide password');
+    }
+  };
+  return (
+    <div className={errors.password ? 'toggle-password error' : 'toggle-password'}>
+      <FontAwesomeIcon icon={icon} className="icon" onClick={togglePasswordView} aria-label={iconLabel} />
+    </div>
+  );
+};
