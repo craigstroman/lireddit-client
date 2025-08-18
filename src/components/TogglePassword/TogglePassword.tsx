d00a2be51b713f5267410de338be3501327805ec
Craig Stroman <cstroman@gmail.com>
2025-08-18 16:34:24 -0400

    Updating toggle password

--- src/components/TogglePassword/TogglePassword.tsx
+++ src/components/TogglePassword/TogglePassword.tsx
@@ -2,28 +2,29 @@ import React, { useState } from 'react';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import { IErrors } from '../../shared/Interfaces';
+import { ITogglePassword } from '../../shared/Interfaces';
 import './TogglePassword.scss';
 
-export const TogglePassword: React.FC<IErrors> = ({ errors }) => {
-  console.log('errors: ', errors);
-  const [fieldType, setFieldType] = useState<string>('password');
+export const TogglePassword: React.FC<ITogglePassword> = ({ errors, onSendValue }) => {
+  const [inputType, setInputType] = useState<string>('password');
   const [icon, setIcon] = useState<IconDefinition>(faEyeSlash);
   const [iconLabel, setIconLabel] = useState<string>('Hide password');
 
   const togglePasswordView = () => {
-    if (fieldType === 'password') {
+    if (inputType === 'password') {
       setIcon(faEye);
-      setFieldType('text');
       setIconLabel('Show password');
+      onSendValue('text');
+      setInputType('text');
     } else {
       setIcon(faEyeSlash);
-      setFieldType('password');
       setIconLabel('Hide password');
+      onSendValue('password');
+      setInputType('password');
     }
   };
   return (
-    <div className={errors.password ? 'toggle-password error' : 'toggle-password'}>
+    <div className={errors?.password ? 'toggle-password error' : 'toggle-password'}>
       <FontAwesomeIcon icon={icon} className="icon" onClick={togglePasswordView} aria-label={iconLabel} />
     </div>
   );
