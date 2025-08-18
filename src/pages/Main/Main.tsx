2f44918bb0680c40276554962a880dd0a89a9781
Craig Stroman <cstroman@gmail.com>
2025-08-18 16:35:42 -0400

    Updating routing

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,7 +3,10 @@ import { Index } from '../Index/Index';
 import { Register } from '../Register/Register';
 import { Login } from '../Login/Login';
 import { Dashboard } from '../Dashboard/Dashboard';
+import { ChangePassword } from '../ChangePassword/ChangePassword';
 import './Main.scss';
+
+// TODO: Figure out how to get route with a parameter working, right now nesting the route doesn't work
 
 export const Main: React.FC = () => {
   return (
@@ -13,6 +16,9 @@ export const Main: React.FC = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/change-password">
+          <Route path=":token" element={<ChangePassword />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
