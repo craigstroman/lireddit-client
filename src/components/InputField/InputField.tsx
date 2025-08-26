56bd2c47c82b9f7fbb64b5e00ec8da5d074af2c4
Craig Stroman <cstroman@gmail.com>
2025-08-26 16:11:23 -0400

    Updating input field component to support text areas

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -3,8 +3,26 @@ import { Field, ErrorMessage } from 'formik';
 import { InputFieldProps } from '../../shared/Interfaces';
 import './InputField.scss';
 
-export const InputField: React.FC<InputFieldProps> = ({ name, placeholder, fieldErrors, type }) => {
+export const InputField: React.FC<InputFieldProps> = ({ name, placeholder, fieldErrors, type, textArea }) => {
   const inputFieldName: string = name;
+
+  if (textArea) {
+    return (
+      <div className="input-container">
+        <Field
+          name={name}
+          placeholder={placeholder}
+          id={name}
+          className={fieldErrors[inputFieldName] ? 'input error' : 'input'}
+          as="textarea"
+        />
+        <div className="error">
+          <ErrorMessage name={name} />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="input-container">
       <Field
