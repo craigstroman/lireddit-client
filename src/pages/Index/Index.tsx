1594d0fde093efcbf4045cc9afc84e420b680fc1
Craig Stroman <cstroman@gmail.com>
2025-08-28 11:53:55 -0400

    Updating index page text

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Layout } from '../../components/Layout/Layout';
-import { usePostsQuery } from '../../generated/graphql';
+import './Index.scss';
 
 export const Index: React.FC = () => {
-  const [{ data }] = usePostsQuery();
   return (
-    <Layout>{!data ? <div>loading...</div> : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}</Layout>
+    <Layout>
+      <h1>Not a registered user? Sign up to create posts</h1>
+    </Layout>
   );
 };
