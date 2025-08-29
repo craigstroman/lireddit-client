1c46ef56961fd50c605f5fc418e38b08fca42053
Craig Stroman <cstroman@gmail.com>
2025-08-29 13:47:42 -0400

    Showing posts on dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useMeQuery } from '../../generated/graphql';
+import { useMeQuery, usePostsQuery } from '../../generated/graphql';
 import { Layout } from '../../components/Layout/Layout';
 import './Dashboard.scss';
 
 export const Dashboard: React.FC = () => {
   const navigate = useNavigate();
   const [{ data }] = useMeQuery();
+  const [{ data: posts }] = usePostsQuery();
 
   if (!data?.me.username) {
     navigate('/');
@@ -14,7 +15,10 @@ export const Dashboard: React.FC = () => {
 
   return (
     <Layout>
-      <div className="content">Dashboard</div>
+      <div className="content">
+        <h1>Dashboard</h1>
+        {posts && posts.posts.map((el) => <div key={el.id}>{el.title}</div>)}
+      </div>
     </Layout>
   );
 };
