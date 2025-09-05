05f93dc84135236b1ffaa62a19c28a1667151a28
Craig Stroman <cstroman@gmail.com>
2025-09-05 12:55:36 -0400

    Updating navbar component

--- src/components/NavBar/NavBar..tsx
+++ src/components/NavBar/NavBar..tsx
@@ -1,37 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMeQuery, useLogoutMutation } from '../../generated/graphql';
-import { IUser } from '../../shared/Interfaces';
 import './NavBar.scss';
 
 export const NavBar: React.FC = () => {
   const [, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
-  const [user, setUser] = useState<IUser | null>(null);
+  const [{ data }] = useMeQuery();
+
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!fetching && data) {
-      if (data.me) {
-        setUser({
-          id: data.me.id,
-          username: data.me.username,
-        });
-      }
-    }
-    setUser(null);
-  }, [fetching]);
-
-  // TODO: Continue figuring out how to rerun meQuery after a user logs out so that the data in data is updated.
-  // TODO: Figure out how to reset cached query because the query is cached
 
   const handleLogout = () => {
     logout();
     navigate('/');
-    setUser(null);
   };
 
-  if (user?.username) {
+  if (data?.me.username) {
     return (
       <header>
         <div className="header-content">
