3ce7c05c847c5151fa28e0439a514fc5874d0454
Craig Stroman <cstroman@gmail.com>
2025-09-05 12:56:54 -0400

    Updating dashboard component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMeQuery, usePostsQuery } from '../../generated/graphql';
 import { Layout } from '../../components/Layout/Layout';
@@ -10,7 +10,7 @@ export const Dashboard: React.FC = () => {
       limit: 10,
     },
   });
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching, error }] = useMeQuery({ requestPolicy: 'network-only' });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,7 +28,7 @@ export const Dashboard: React.FC = () => {
         <div className="create-post-link">
           <a href="/create-post">Create a post</a>
         </div>
-        {posts && posts.posts.map((el) => <div key={el.id}>{el.title}</div>)}
+        {posts && posts.posts.map((el: any) => <div key={el.id}>{el.title}</div>)}
       </div>
     </Layout>
   );
