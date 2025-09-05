074dc92a76801eb18b424afe51b22673f4c33b2a
Craig Stroman <cstroman@gmail.com>
2025-09-05 15:34:11 -0400

    Removing to do comment, adding debugExchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
+import { Client, Provider, cacheExchange, fetchExchange, dedupExchange, Exchange } from 'urql';
+import { pipe, tap } from 'wonka';
 import { Main } from './pages/Main/Main';
 
 const element = document.getElementById('app');
 const root = createRoot(element as HTMLDivElement);
+
+const debugExchange: Exchange =
+  ({ forward }: any) =>
+  (ops$: any) =>
+    pipe(
+      forward(ops$),
+      tap((e) => console.log('[urql]', e)),
+    );
 
 const client = new Client({
   url: 'http://localhost:9000/graphql',
   fetchOptions: {
     credentials: 'include',
   },
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [dedupExchange, cacheExchange, debugExchange, fetchExchange],
 });
 
 root.render(
