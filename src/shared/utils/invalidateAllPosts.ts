201825d5792786a90508a190822da9cad9719390
Craig Stroman <cstroman@gmail.com>
2025-09-10 20:44:00 -0400

    Creating invalidate all posts method

---
+++ src/shared/utils/invalidateAllPosts.ts
@@ -0,0 +1,9 @@
+import { Cache } from '@urql/exchange-graphcache';
+
+export const invalidateAllPosts = (cache: Cache) => {
+  const allFields = cache.inspectFields('Query');
+  const fieldInfos = allFields.filter((info) => info.fieldName === 'posts');
+  fieldInfos.forEach((fi) => {
+    cache.invalidate('Query', 'posts', fi.arguments || {});
+  });
+};
