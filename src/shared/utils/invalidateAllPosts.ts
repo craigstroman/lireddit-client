0a6eb8d841dc71b2b629347a0d4d154a4e4051dd
Craig Stroman <cstroman@gmail.com>
2025-09-15 12:31:19 -0400

    Deleting a file

--- src/shared/utils/invalidateAllPosts.ts
+++
@@ -1,9 +0,0 @@
-import { Cache } from '@urql/exchange-graphcache';
-
-export const invalidateAllPosts = (cache: Cache) => {
-  const allFields = cache.inspectFields('Query');
-  const fieldInfos = allFields.filter((info) => info.fieldName === 'posts');
-  fieldInfos.forEach((fi) => {
-    cache.invalidate('Query', 'posts', fi.arguments || {});
-  });
-};
