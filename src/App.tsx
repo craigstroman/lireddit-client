fdc2be61eb3c3bcd1790d83f0876f4d758d24f65
Craig Stroman <cstroman@gmail.com>
2025-09-16 12:46:54 -0400

    Updating app configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Client, Provider, cacheExchange, fetchExchange, dedupExchange, Exchange } from 'urql';
+import { Client, Provider, fetchExchange, Exchange } from 'urql';
+// dedupExchange
+import { cacheExchange } from '@urql/exchange-graphcache';
 import { pipe, tap } from 'wonka';
 import { Main } from './pages/Main/Main';
+import { invalidateAllPosts } from './shared/utils/invalidateAllPosts';
+import { betterUpdateQuery } from './shared/utils/betterUpdateQuery';
+import { MeDocument, MeQuery, LogoutMutation } from './generated/graphql';
 
 const element = document.getElementById('app');
 const root = createRoot(element as HTMLDivElement);
-
-// TODO: Continue trying to figure out how to return a object for cacheExchange to create a new client because that's how Adam does it in the tutorial he just returns an object for the client
 
 const debugExchange: Exchange =
   ({ forward }: any) =>
@@ -22,7 +25,28 @@ const client = new Client({
   fetchOptions: {
     credentials: 'include',
   },
-  exchanges: [dedupExchange, cacheExchange, debugExchange, fetchExchange],
+  exchanges: [
+    cacheExchange({
+      keys: {},
+      updates: {
+        Mutation: {
+          createPost: (_result, args, cache, info) => {
+            invalidateAllPosts(cache);
+          },
+          // TODO: Figure out why this I can't set me to null
+          // TODO: Figure out how to get logout function working
+          // TODO: This is because now I am using latest version of urql
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(cache, { query: MeDocument }, _result, () => ({
+              me: null,
+            }));
+          },
+        },
+      },
+    }),
+    debugExchange,
+    fetchExchange,
+  ],
 });
 
 root.render(
