ffa3b9430d52d082af47d8616274b5100ec119de
Craig Stroman <cstroman@gmail.com>
2025-10-02 15:02:25 -0400

    Updating port of GraphQL server

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const nodeEnv = process.env.NODE_ENV;
 
 const client = new Client({
   url:
-    nodeEnv === 'production' ? 'http://lireddit.craigstroman.com/graphql' : 'http://localhost:9000/graphql',
+    nodeEnv === 'production' ? 'http://lireddit.craigstroman.com/graphql' : 'http://localhost:9008/graphql',
   fetchOptions: {
     credentials: 'include',
   },
