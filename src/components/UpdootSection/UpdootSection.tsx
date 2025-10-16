7e70593e2eaa25349f06262ecc805535674b865e
Craig Stroman <cstroman@gmail.com>
2025-10-16 16:19:48 -0400

    Updating updoot markup

--- src/components/UpdootSection/UpdootSection.tsx
+++ src/components/UpdootSection/UpdootSection.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import './UpdootSection.scss';
 
 export const UpdootSection: React.FC = () => {
   return (
-    <div className="d-flex flex-row mb-2">
-      <div className="p2">
+    <div className="updoot-container">
+      <div className="arrow-container">
         <FontAwesomeIcon icon={faAngleUp} className="icon" aria-label="Vote Up" />
       </div>
-      <div className="p2">
+      <div className="arrow-container">
         <FontAwesomeIcon icon={faAngleDown} className="icon" aria-label="Vote Down" />
       </div>
     </div>
