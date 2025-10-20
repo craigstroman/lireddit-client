0675d4b564138960edb1c179e4a84c6189f49afc
Craig Stroman <cstroman@gmail.com>
2025-10-20 15:22:30 -0400

    Adding points to UpdootSection component

--- src/components/UpdootSection/UpdootSection.tsx
+++ src/components/UpdootSection/UpdootSection.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { PostSnippetFragment, useVoteMutation } from '../../generated/graphql';
 import './UpdootSection.scss';
 
-export const UpdootSection: React.FC = () => {
+interface UpdootSectionProps {
+  post: PostSnippetFragment;
+}
+
+export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<'updoot-loading' | 'downdoot-loading' | 'not-loading'>(
+    'not-loading',
+  );
+  const [, vote] = useVoteMutation();
+
   return (
     <div className="updoot-container">
       <div className="arrow-container">
         <FontAwesomeIcon icon={faAngleUp} className="icon" aria-label="Vote Up" />
       </div>
+      <div className="point-container">{post.points}</div>
       <div className="arrow-container">
         <FontAwesomeIcon icon={faAngleDown} className="icon" aria-label="Vote Down" />
       </div>
