4f3e05db8c00f6d27fb2b7db8da8a19c3dbd81e7
Craig Stroman <cstroman@gmail.com>
2025-10-21 10:38:26 -0400

    Adding method to handle up voting of posts

--- src/components/UpdootSection/UpdootSection.tsx
+++ src/components/UpdootSection/UpdootSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { PostSnippetFragment, useVoteMutation } from '../../generated/graphql';
@@ -8,16 +8,27 @@ interface UpdootSectionProps {
   post: PostSnippetFragment;
 }
 
+// TODO: Fix error that occurs when I click on upvote
+
 export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
-  const [loadingState, setLoadingState] = useState<'updoot-loading' | 'downdoot-loading' | 'not-loading'>(
-    'not-loading',
-  );
   const [, vote] = useVoteMutation();
+
+  const handleUpVote = async () => {
+    await vote({
+      postId: post.id,
+      value: 1,
+    });
+  };
 
   return (
     <div className="updoot-container">
       <div className="arrow-container">
-        <FontAwesomeIcon icon={faAngleUp} className="icon" aria-label="Vote Up" />
+        <FontAwesomeIcon
+          icon={faAngleUp}
+          className="icon"
+          aria-label="Vote Up"
+          onClick={async () => handleUpVote()}
+        />
       </div>
       <div className="point-container">{post.points}</div>
       <div className="arrow-container">
