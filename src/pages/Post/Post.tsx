9837bc8667ad8a5ff87b8bdbaf80760913b0c8ac
Craig Stroman <cstroman@gmail.com>
2025-11-07 12:55:37 -0500

    Creating a single post page

---
+++ src/pages/Post/Post.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Layout } from '../../components/Layout/Layout';
+import { usePostQuery } from '../../generated/graphql';
+
+export const Post: React.FC = () => {
+  const { id } = useParams();
+  let idParam = null;
+
+  if (id) {
+    idParam = parseFloat(id);
+  }
+
+  const [{ data, error, fetching }] = usePostQuery({
+    pause: idParam === -1,
+    variables: {
+      id: idParam ? idParam : 0,
+    },
+  });
+
+  if (fetching) {
+    return <Layout>Loading...</Layout>;
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <div>{error.message}</div>
+      </Layout>
+    );
+  }
+
+  if (data && data.post) {
+    return (
+      <Layout>
+        <h1>{data.post.title}</h1>
+        <div>{data.post.text}</div>
+      </Layout>
+    );
+  }
+};
