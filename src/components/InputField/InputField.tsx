b762d439f39e5ecb9c05732d7408a4daf776762d
Craig Stroman <cstroman@gmail.com>
2025-11-13 12:58:52 -0500

    Adding show label and value properties to input field component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -3,18 +3,28 @@ import { Field, ErrorMessage } from 'formik';
 import { InputFieldProps } from '../../shared/Interfaces';
 import './InputField.scss';
 
-export const InputField: React.FC<InputFieldProps> = ({ name, placeholder, fieldErrors, type, textArea }) => {
+export const InputField: React.FC<InputFieldProps> = ({
+  name,
+  placeholder,
+  fieldErrors,
+  type,
+  textArea,
+  value,
+  showLabels,
+}) => {
   const inputFieldName: string = name;
 
   if (textArea) {
     return (
       <div className="input-container">
+        {showLabels && <label htmlFor={name}>{placeholder}</label>}
         <Field
           name={name}
           placeholder={placeholder}
           id={name}
-          className={fieldErrors[inputFieldName] ? 'input error' : 'input'}
+          className={fieldErrors[inputFieldName] ? 'textarea error' : 'textarea'}
           as="textarea"
+          value={value}
         />
         <div className="error">
           <ErrorMessage name={name} />
@@ -25,12 +35,14 @@ export const InputField: React.FC<InputFieldProps> = ({ name, placeholder, field
 
   return (
     <div className="input-container">
+      {showLabels && <label htmlFor={name}>{placeholder}</label>}
       <Field
         type={type?.length ? type : 'text'}
         name={name}
         placeholder={placeholder}
         id={name}
         className={fieldErrors[inputFieldName] ? 'input error' : 'input'}
+        value={value}
       />
       <div className="error">
         <ErrorMessage name={name} />
